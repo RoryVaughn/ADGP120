c428a578beafb2c98d0f67e8f19774b84410cb86
RvaughnAIEYear1 <Rvaughn2525@gmail.com>
2016-04-19 12:02:16 -0500

    Still cant spawn boxes.. :C

--- Astar.py
+++ Astar.py
@@ -4,12 +4,38 @@ BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 GREEN = (0, 255, 0)
 RED = (255, 0, 0)
-width = 20
-height = 20
-margin = 5
-left = (margin + width) *  margin
-top = (margin + height) *  margin
 
+class Node:
+	def __init__(self, x, y):
+		self.parent = None		
+		self.color = WHITE
+		self.width = 20
+		self.height = 20
+		self.margin = 5
+		self.left = (self.margin + self.width) *  x + self.margin
+		self.top = (self.margin + self.height) *  y + self.margin
+		self.walkable = True
+		self.pos = (x, self.height - y)
+		self.f = None
+		self.g = None
+		self.h = None
+
+	def draw(self, screen, color):
+		margin = self.margin
+		color = (0, 0, 255) if (self.walkable) else (255,0,0)
+		gfx.draw.rect(screen, color, (self.left , self.top, self.width, self.height))
+		
+	def setWalk(self, walkable):
+		self.walkable = walkable
+		 
+	def getF(self):
+		return self.h + self.g
+	def setH(self, val):
+		self.h = val
+	def setG(self, val):
+		self.g = val
+
+		
 pygame.init()
  
 # Set the width and height of the screen [width, height]
@@ -31,7 +57,7 @@ while not done:
 	#
 	
     for event in pygame.event.get():
-	pygame.draw.rect(screen, WHITE, (left , top, width, height))
+	
 	
         if event.type == pygame.QUIT:
             done = True
